bd23d6e209d7da3150880f3b71c52ce62ed83f6b
xSergioz <1698224@alu.murciaeduca.es>
2024-11-14 13:59:02 +0100

    cambio de juego

--- Juego/JuegoCliente.js
+++ Juego/JuegoCliente.js
@@ -6,23 +6,54 @@ window.onload = function () {
 	const TOPABAJO = 473;
 
     let x = 0;
-    let y = 250;
+    let y = 50;
     let ctx;
+    let posicion = 0;
     let canvas;
+    let imagen;
+    let miThorfinn;
+    let id1;
 
-    function Avion (x_, y_) {
+    function Thorfinn (x_, y_) {
 
         this.x = x_;
         this.y = y_;
+        this.animacionThorfinn = [[9,211],[31,211]];
         this.velocidad = 10;
-        this.tamañoX = 30;
-        this.tamañoY = 30;
+        this.tamañoX = 20;
+        this.tamañoY = 45;
     }
 
-    function crearAvion() {
-        
-    }
+    function crearThorfinn() {
+
+      ctx.clearRect(0, 0, 500, 500);
+        //console.log(miThorfinn.imagen);
+        //console.log(miThorfinn.x, " - ",miThorfinn.y);
+
+        ctx.drawImage(miThorfinn.imagen,
+                      miThorfinn.animacionThorfinn[posicion][0],
+                      miThorfinn.animacionThorfinn[posicion][1],
+                      miThorfinn.tamañoX,
+					  miThorfinn.tamañoY,
+					  miThorfinn.x,
+					  miThorfinn.y,
+					  miThorfinn.tamañoX,
+					  miThorfinn.tamañoY);			  
+
+	}
+
 
     canvas = document.getElementById("miCanvas");
     ctx = canvas.getContext("2d");
+
+
+    imagen = new Image();
+	imagen.src="/home/alumno/Documentos/GitHub/VideojuegoCliente/Assets/img/spriteThorfinn.png";
+
+//    imagen.src="248259.png";
+	Thorfinn.prototype.imagen = imagen;
+
+	miThorfinn = new Thorfinn( x, y);
+
+    id1= setInterval(crearThorfinn, 1000/50);
 }
