0411bdc05bd0443292331d039447bca45a74c5fc
xSergioz <1698224@alu.murciaeduca.es>
2024-11-20 19:13:33 +0100

    Trabajo Tarde Casa
    Toda la tarde para sacar la formula del salto, con el limite visual del suelo, y meterle un countdown al salto, tomo un descanso que ando frito, veremos a ver si me pongo luego.
    
    Siguiente paso, meterle la animacion de salto del sprite cuando salte, y empezar a meter 2 plataformas.

--- Juego/JuegoCliente.js
+++ Juego/JuegoCliente.js
@@ -1,14 +1,15 @@
 window.onload = function () {
 
     const TOPEDERECHA = 280;
-	const TOPIZQUIERDA = 0;
-	const TOPARRIBA = 0;
-	const TOPABAJO = 473;
-    const gravedad = 0;
-    const Vsalto = 15;
+    const TOPIZQUIERDA = 0;
+    const TOPARRIBA = 0;
+    const TOPABAJO = 140;
+    const gravedad = 1.8;
+    let Vsalto = 15;
 
     let x = 0;
     let y = 100;
+    const alturaSuelo = 98;
 
     let ctx;
     let posicion = 0;
@@ -22,9 +23,12 @@ window.onload = function () {
 
     let xIzquierda, xDerecha;
     let salto;
+    let enElAire = false;
+    let puedeSaltar = true;
+
+    let CDTimer;
 
     function Thorfinn (x_, y_) {
-
         this.x = x_;
         this.y = y_;
         this.animacionThorfinn = [[4,202],[26,202]];
@@ -35,153 +39,144 @@ window.onload = function () {
     }
 
     Thorfinn.prototype.generaPosicionDerecha = function () {
-
         this.x = this.x + this.velocidad;
         if (this.x > TOPEDERECHA) {
-			
-			this.x = TOPEDERECHA;   
-		}
+            this.x = TOPEDERECHA;   
+        }
         this.animacionThorfinnDerecha = [[0,261],[56,261],[113,261],[167,261],[224,261],[283,261]];
         this.animacionThorfinn = this.animacionThorfinnDerecha;
     }
 
     Thorfinn.prototype.generaPosicionIzquierda = function() {
-		
-		this.x = this.x - this.velocidad;;
-
-		if (this.x < TOPIZQUIERDA) {
-			
-			this.x = TOPIZQUIERDA;	   
-		}
+        this.x = this.x - this.velocidad;
+        if (this.x < TOPIZQUIERDA) {
+            this.x = TOPIZQUIERDA;   
+        }
         this.animacionThorfinnIzquierda = [[302,682],[246,682],[188,682],[133,682],[77,682],[21,682]];
         this.animacionThorfinn = this.animacionThorfinnIzquierda;
-	}
-
-    /*Thorfinn.prototype.generaPosicionSalto = function() {
-
-        if(!saltando) return;
-
-        this.velocidad -= gravedad;
-        this.y -= this.velocidad;
-
-        if (this.y >= TOPARRIBA) {
-            this.y = TOPARRIBA;
-            saltando = false;
-            this.velocidad = 0;
-        }
     }
-    */
-    function crearThorfinn() {
 
-      ctx.clearRect(0, 0, 500, 500);
+    Thorfinn.prototype.generaPosicionSalto = function () {
+        if (enElAire || !puedeSaltar) {
+            return;
+        }
+
+        enElAire = true;
+        Vsalto = 15;
+        
+        let saltoInterval = setInterval(() => {
+            this.y -= Vsalto;
+            Vsalto -= gravedad;
+
+            if (this.y >= TOPABAJO - this.tamañoY) {
+                this.y = TOPABAJO - this.tamañoY;
+                enElAire = false; 
+                clearInterval(saltoInterval);
+                
+                this.y = alturaSuelo;
+
+                iniciarCD();
+            }
+        }, 20); 
+    }
+
+    function iniciarCD() {
+        puedeSaltar = false;
+        let CD = 3;
+    
+        CDTimer = setInterval(() => {
+            CD--;
+    
+            if (CD < 1) {
+                clearInterval(CDTimer);
+                puedeSaltar = true;
+            }
+        }, 1000);
+    }
+
+    function crearThorfinn() {
+        ctx.clearRect(0, 0, 500, 500);
 
         if (xDerecha) {
-			
             miThorfinn.generaPosicionDerecha();
         }
 
         if (xIzquierda) {
-
             miThorfinn.generaPosicionIzquierda();
         }
 
-        /*if (salto) {
-            miThorfinn.generaPosicionSalto();
-        }
-        */
-        ctx.drawImage(miThorfinn.imagen,
-                      miThorfinn.animacionThorfinn[posicion][0],
-                      miThorfinn.animacionThorfinn[posicion][1],
-                      miThorfinn.tamañoX,
-					  miThorfinn.tamañoY,
-					  miThorfinn.x,
-					  miThorfinn.y,
-					  miThorfinn.tamañoX,
-					  miThorfinn.tamañoY);
+        if (miThorfinn.y > TOPABAJO - miThorfinn.tamañoY) {
+            miThorfinn.y = TOPABAJO - miThorfinn.tamañoY;
 
-	}
+            enElAire = false;
+           
+            miThorfinn.y = alturaSuelo;
+        }
+
+        ctx.drawImage(miThorfinn.imagen,
+            miThorfinn.animacionThorfinn[posicion][0],
+            miThorfinn.animacionThorfinn[posicion][1],
+            miThorfinn.tamañoX,
+            miThorfinn.tamañoY,
+            miThorfinn.x,
+            miThorfinn.y,
+            miThorfinn.tamañoX,
+            miThorfinn.tamañoY
+        );
+    }
 
     function animacionThorfinn() {
-
         if (xDerecha) {
             posicion = (posicion + 1) % 6;
             miThorfinn.animacionThorfinn = miThorfinn.animacionThorfinnDerecha;
-        }
-        else if (xIzquierda) {
+        } else if (xIzquierda) {
             posicion = (posicion + 1) % 6;
             miThorfinn.animacionThorfinn = miThorfinn.animacionThorfinnIzquierda;
         } else {
             miThorfinn.animacionThorfinn = miThorfinn.animacionIddle;
             posicion = (posicion + 1) % 4;
         }
-
     }
 
-    /*function saltar() {
-
-        if(!saltando) return;
-
-        this.velocidad -= gravedad;
-        this.y -= this.velocidad;
-
-        if (this.y >= 100) {
-            this.y = 100;
-            saltando = false;
-            this.velocidad = 0;
-        }
-
-    }
-    */
     function activaMovimiento(evt) {
-
         switch (evt.keyCode) {
-		
-			case 37: 
-			  xIzquierda = true;
-			  break;
-			case 39:
-			  xDerecha = true;
-			  break;
-
-            case 32:
-                salto = true;
+            case 37: 
+                xIzquierda = true;
                 break;
-		 
-		}
-	}
-
-	function desactivaMovimiento(evt){
-
-        switch (evt.keyCode) {
-
-			case 37: 
-			  xIzquierda = false;
-			  break;
-
-			case 39:
-			  xDerecha = false;
-			  break;
-
+            case 39:
+                xDerecha = true;
+                break;
             case 32:
-                salto = false;
+                if (!enElAire && puedeSaltar) {
+                    miThorfinn.generaPosicionSalto();
+                }
                 break;
         }
-	}
+    }
 
+    function desactivaMovimiento(evt) {
+        switch (evt.keyCode) {
+            case 37: 
+                xIzquierda = false;
+                break;
+            case 39:
+                xDerecha = false;
+                break;
+        }
+    }
 
     document.addEventListener("keydown", activaMovimiento, false);
-	document.addEventListener("keyup", desactivaMovimiento, false);
+    document.addEventListener("keyup", desactivaMovimiento, false);
 
     canvas = document.getElementById("miCanvas");
     ctx = canvas.getContext("2d");
 
-
     imagen = new Image();
-	imagen.src="../Juego/Assets/img/spriteThorfinn.png";
+    imagen.src = "../Juego/Assets/img/spriteThorfinn.png";
 
-	Thorfinn.prototype.imagen = imagen;
+    Thorfinn.prototype.imagen = imagen;
 
-	miThorfinn = new Thorfinn(x, y);
+    miThorfinn = new Thorfinn(x, y);
     id1 = setInterval(crearThorfinn, 1000 / 90);
     id2 = setInterval(animacionThorfinn, 1000 / 10);
 }
